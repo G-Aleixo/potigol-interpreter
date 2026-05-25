39e8a2a1dd6f7538471d648c885bc3c959b76591
G-Aleixo <g.aleixo@escolar.ifrn.edu.br>
2026-05-25 14:18:18 -0300

    feat: main now interprets from file given

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,12 +1,30 @@
+use std::{env::args, io::Read};
+
 use potigol_interpreter::{interpreter::walker::Interpreter, lexer::tokenize, parser};
 
 fn main() {
-    let tokens = tokenize(include_str!("../../test.poti")).unwrap();
+    let args = args();
+    let mut filepath = None;
 
-    println!("{tokens:#?}");
-    println!("{:#?}", parser::Parser::new(tokens.clone()).parse().unwrap());
+    for (i, arg) in args.enumerate() {
+        if i > 1 {
+            panic!("Expected only one argument");
+        }
+        if i == 1 {
+            filepath = Some(arg);
+        }
+    }
+    let filepath = filepath.expect("Expected filename to interpret");
+
+    let mut file = std::fs::File::open(filepath).expect("File doesn't exist");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).expect("Could not read from file");
+
+    let tokens = tokenize(&contents).unwrap();
+
+    let ast = parser::Parser::new(tokens.clone()).parse().unwrap();
 
     let mut interp = Interpreter::new();
 
-    interp.interpret(parser::Parser::new(tokens).parse().unwrap());
+    interp.interpret(ast);
 }
